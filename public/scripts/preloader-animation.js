55034e0dd77b336140f65b4c6531ec18dec3bbb8
uroborosq <uroborosq@ya.ru>
2022-11-23 14:18:22 +0300

    fix: add necessary dependencies

---
+++ public/scripts/preloader-animation.js
@@ -0,0 +1,17 @@
+function applyPreloaderAnimation()
+{
+
+    const options = {
+        classname: 'my-class',
+        id: 'my-id',
+    };
+
+    const nanobar = new Nanobar(options);
+
+    nanobar.go( 30 );
+    nanobar.go( 76 );
+    nanobar.go(100);
+
+}
+
+

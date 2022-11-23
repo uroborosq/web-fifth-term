912021249c6153be8bbcfd21c91afc100fa8a55e
uroborosq <uroborosq@ya.ru>
2022-11-23 14:20:58 +0300

    fix: add explanations for lab

--- public/scripts/preloader-animation.js
+++ public/scripts/preloader-animation.js
@@ -1,13 +1,11 @@
 function applyPreloaderAnimation()
 {
+    // add nice page loading bar to the top of the page
 
-    const options = {
-        classname: 'my-class',
-        id: 'my-id',
-    };
+    // this creating a bar object without any additional options
+    const nanobar = new Nanobar();
 
-    const nanobar = new Nanobar(options);
-
+    // every command sets progress of the bar to imitate loading
     nanobar.go( 30 );
     nanobar.go( 76 );
     nanobar.go(100);
